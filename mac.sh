6fd6096441338f29b9632e1c1cd183c2a22a5f02
fmaj7 <fmaj7@live.com>
2014-12-28 09:23:42 -0800

    Adding more application from brew cask

--- mac.sh
+++ mac.sh
@@ -5,6 +5,23 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # HomeBrew Cask http://caskroom.io/
 brew install caskroom/cask/brew-cask
 
+# Alfred
+brew cask install alfred
+# To Link Alfred 2 with Caskroom without buying Power Pack
+# https://github.com/caskroom/homebrew-cask/issues/8052#issuecomment-68149673
+# https://github.com/caskroom/homebrew-cask/issues/5820#issuecomment-61175615
+
+# Google Chrome
+brew cask install google-chrome
+# preference comes from syncing
+
+# Firefox
+brew cask install firefox
+
+# iTerm 2
+brew cask install iterm2
+# How about preferences?
+
 # Virtual Box - Docker requirement 
 brew cask install virtualbox
 
