f5bf30bcfa498a7e70bfd81a8bdf4b67cccecb12
fmaj7 <fmaj7@users.noreply.github.com>
2015-01-05 16:51:21 -0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,4 +1,3 @@
-
 # HomeBrew - http://brew.sh/
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -6,25 +5,41 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 
 # Tap Alternative Versions of Casks
-# https://github.com/caskroom/homebrew-versions
+## https://github.com/caskroom/homebrew-versions
 brew tap caskroom/versions
 
 # Alfred
 brew cask install alfred
-# To Link Alfred 2 with Caskroom without buying Power Pack
-# https://github.com/caskroom/homebrew-cask/issues/8052#issuecomment-68149673
-# https://github.com/caskroom/homebrew-cask/issues/5820#issuecomment-61175615
+## To Link Alfred 2 with Caskroom without buying Power Pack
+## https://github.com/caskroom/homebrew-cask/issues/8052#issuecomment-68149673
+## https://github.com/caskroom/homebrew-cask/issues/5820#issuecomment-61175615
+
+# Better Touch Tool
+brew cask install bettertouchtool
+
+# Spectacle
+brew cask install spectacle
 
 # Google Chrome
 brew cask install google-chrome
-# preference comes from syncing
+## preference comes from syncing
+## ~/Library/Application Support/Google/Chrome/(Default|Profile 1)
 
 # Firefox
 brew cask install firefox
+## preferences?
 
 # iTerm 2
 brew cask install iterm2
-# How about preferences?
+## How about preferences?
+## Add/Change font: Consolas
+## Profiles
+## Preferences
+
+# Sublime Text
+brew cask install sublime-text3
+## MarkdownEditing
+## Change key binding super+f => show pannel replace
 
 # Virtual Box - Docker requirement 
 brew cask install virtualbox
@@ -33,3 +48,15 @@ brew cask install virtualbox
 brew install docker
 brew install boot2docker
 boot2docker init
+
+# dotfiles
+## zprezto
+## zip -r --symlinks dotfiles.zip .ssh .zprezto .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc* .zsh_history .zsh-update .gitconfig .history .android .adium_sync .tmux.conf .vim .vimrc .viminfo .zhistory
+
+# Apple XCode Commmandline Tools
+
+# JDK
+## 1.7
+## 1.8
+
+# Intelij
