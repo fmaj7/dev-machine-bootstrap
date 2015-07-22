452276d4e7b8419dbacb18785532c9dcefbdac02
fmaj7 <fmaj7@users.noreply.github.com>
2015-07-21 23:12:40 -0700

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -11,11 +11,21 @@ brew install caskroom/cask/brew-cask
 ## https://github.com/caskroom/homebrew-versions
 brew tap caskroom/versions
 
+brew cask install spectacle
+open -a spectacle
+
+brew cask install bettertouchtool
+open -a bettertouchtool
+
 brew cask install dropbox
+open -a dropbox
+
 brew cask install evernote
+open -a evernote
 
 # Preferences come form google account sync
 brew cask install google-chrome
+open -a google\ chrome
 
 brew cask install firefox
 ## preferences?
@@ -25,9 +35,6 @@ brew cask install alfred
 ## To Link Alfred 2 with Caskroom without buying Power Pack
 ## https://github.com/caskroom/homebrew-cask/issues/8052#issuecomment-68149673
 ## https://github.com/caskroom/homebrew-cask/issues/5820#issuecomment-61175615
-
-brew cask install bettertouchtool
-brew cask install spectacle
 
 # iTerm 2
 brew cask install iterm2
@@ -41,7 +48,8 @@ brew cask install sublime-text3
 ## MarkdownEditing
 ## Change key binding super+f => show pannel replace
 
-# Intelij
+# JDK
+brew cask install java6 java7 java
 
 # Docker
 brew cask install virtualbox
@@ -49,12 +57,10 @@ brew install docker
 brew install boot2docker
 boot2docker init
 
+# Intelij
+brew cask install intellij-idea
+open -a intellij\ idea\ 14
+
 # dotfiles
 ## zprezto
 ## zip -r --symlinks dotfiles.zip .ssh .zprezto .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc* .zsh_history .zsh-update .gitconfig .history .android .adium_sync .tmux.conf .vim .vimrc .viminfo .zhistory
-
-# Apple XCode Commmandline Tools
-
-# JDK
-## 1.7
-## 1.8
