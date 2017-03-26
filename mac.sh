366138a1c65709ecad3f78c77fa0ede7ad240a66
fmaj7 <fmaj7@users.noreply.github.com>
2017-03-26 12:09:36 -0700

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,24 +1,25 @@
 # HomeBrew - http://brew.sh/
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Already come in with OS
+## Already come in with OS
 # brew install git
 
-brew install fish
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
-
-# ZSH
-## brew install zsh
-## chsh -s /bin/zsh
-## zprezto
-## zip -r --symlinks dotfiles.zip .ssh .zprezto .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc* .zsh_history .zsh-update .gitconfig .history .android .adium_sync .tmux.conf .vim .vimrc .viminfo .zhistory
-
-brew install docker
-brew install boot2docker
-boot2docker init
-
-# Nodejs
+## Nodejs
 brew install node
 
-brew install coreutils ag
+## GNU core utils
+brew install coreutils 
+
+## CLI Utils I need
+brew install ag
+
+## direnv - https://github.com/direnv/direnv
+brew install direnv
+## Should be set in ZSH/pretzo setup step below
+# echo 'eval "$(direnv hook zsh)"'
+
+## ZSH
+# brew install zsh
+# chsh -s /bin/zsh
+# zprezto
+# zip -r --symlinks dotfiles.zip .ssh .zprezto .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc* .zsh_history .zsh-update .gitconfig .history .android .adium_sync .tmux.conf .vim .vimrc .viminfo .zhistory
